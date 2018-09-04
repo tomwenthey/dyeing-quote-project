4e6c70288afc9dbbddf24b144d409560e9d9f0d8
tomwenthey <a429273392@qq.com>
2018-09-04 14:10:21 +0800

    change sth.

--- dyeing-quote-server/app.js
+++ dyeing-quote-server/app.js
@@ -1,26 +1,59 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var favicon = require("serve-favicon");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var cors = require("cors");
+var mongoose = require("mongoose");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var index = require("./routes/index");
+var users = require("./routes/users");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+// DB connection through Mongoose
+const options = {
+  keepAlive: 30000,
+  connectTimeoutMS: 30000,
+  useNewUrlParser: true
+}; // Just a bunch of options for the db connection
+mongoose.Promise = global.Promise;
+// Don't forget to substitute it with your connection string
+mongoose.connect(
+  "mongodb://localhost/dyeingquote",
+  options
+);
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", index);
+
+// user api
+app
+  .route("/users")
+  .get(users.getUsers)
+  .post(users.createUser);
+
+app
+  .route("/users/:id")
+  .get(users.getUser)
+  .post(users.updateUser);
+
+app.post("/users/login", users.userLogin);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -31,11 +64,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- dyeing-quote-server/routes/users.js
+++ dyeing-quote-server/routes/users.js
@@ -1,9 +1,43 @@
-var express = require('express');
-var router = express.Router();
+const UserModel = require("../models/users");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const userLogin = (req, res) => {
+  res.json({ message: "hello" });
+};
 
-module.exports = router;
+const getUsers = (req, res) => {
+  UserModel.find(null, null, (err, users) => {
+    if (err) {
+      res.send(err);
+    }
+
+    res.json(users);
+  });
+};
+
+const getUser = (req, res) => {
+  res.json({ message: "hello" });
+};
+
+const updateUser = (req, res) => {
+  res.json({ message: "hello" });
+};
+
+const createUser = (req, res) => {
+  let UserEntity = new UserModel(req.body);
+
+  UserEntity.save(err => {
+    if (err) {
+      res.json({ status: 0, message: err });
+    }
+
+    res.json({ status: 1, message: "user created" });
+  });
+};
+
+module.exports = {
+  userLogin,
+  getUsers,
+  getUser,
+  updateUser,
+  createUser
+};
